9a3de2cdca74b9f2217346f97ea23a9e27798c04
Shimapondo <williamnamabende@gmail.com>
2023-07-25 12:36:30 -0700

    Function which calculates and returns the size of specifier

---
+++ get_size.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_size - Does the Calculation of the size to cast the argument.
+ * @format:Gives the format string in which to print the arguments.
+ * @j: Provides a list of arguments to be printed.
+ *
+ * Return: Precision on success.
+ */
+
+int get_size(const char *format, int *j)
+{
+	int curr_j = *j + 1;
+	int size = 0;
+
+	if (format[curr_j] == 'l')
+		size = S_LONG;
+	else if (format[curr_j] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*j = curr_j - 1;
+	else
+		*j = curr_j;
+
+	return (size);
+}
+
